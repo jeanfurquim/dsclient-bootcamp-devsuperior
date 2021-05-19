2fd8b8aab44f6307af0b7d4ecbe36e2e9c3c6920
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-05-19 02:47:08 -0300

    Find by category, Exception handling

--- backend/src/main/java/com/jeanfurquim/dsclient/services/ClientService.java
+++ backend/src/main/java/com/jeanfurquim/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.jeanfurquim.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +11,26 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jeanfurquim.dsclient.dto.ClientDTO;
 import com.jeanfurquim.dsclient.entities.Client;
 import com.jeanfurquim.dsclient.repositories.ClientRepository;
+import com.jeanfurquim.dsclient.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
-		
+
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
-		
-		
+
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}	
 }
